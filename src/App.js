d3fafe3aeaab8abc9ec72f51185dadc212fc4cd4
Brandon Ha <hsy0602@outlook.com>
2020-11-30 15:44:12 +0900

    32. Creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
        <h1>hello</h1>
+       <Person/>
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Person =() =>  {
+    return (
+        <div>
+            <p>I'm person</p>
+        </div>
+    )
+}
+
+export default Person
