c7e6a0407a0eb18885792a2c1664607a40a9dfcb
Brandon Ha <hsy0602@outlook.com>
2020-11-30 16:48:13 +0900

    36. working with props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import Person from './Person/Person'
+import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
-       <h1>hello</h1>
-       <Person/>
-       <Person/>
-       <Person/>
+      <h1>hello</h1>
+      <Person name="rachel" age="24" />
+      <Person name="morgan" age="20">
+        Some hobby : Racing
+      </Person>
+      <Person name="emily" age="23" />
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Person =() =>  {
-    return (
-        <div>
-            <p>I'm person and {Math.floor(Math.random() * 30)} years old</p>
-        </div>
-    )
-}
+const Person = (props) => {
+  return (
+    <div>
+      <p>
+        I'm {props.name} and {props.age} years old
+      </p>
+    </div>
+  );
+};
 
-export default Person
+export default Person;
