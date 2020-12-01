5fd58efcbb7140645209165759bb116b16a285ce
Brandon Ha <hsy0602@outlook.com>
2020-12-01 18:03:02 +0900

    69 Using Radium for media Queries

--- course/src/Person/Person.js
+++ course/src/Person/Person.js
@@ -3,8 +3,14 @@ import Radium from "radium";
 import "./Person.css";
 
 const Person = (props) => {
+  const style = {
+    "@media (min-width:500px) ": {
+      width: "450px",
+    },
+  };
+
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>
         I'm {props.name} and {props.age} years old
       </p>
