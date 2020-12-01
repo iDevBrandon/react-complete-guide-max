993ce0bc7529e06e6be474313b252c17bb2a181e
Brandon Ha <hsy0602@outlook.com>
2020-12-01 16:04:33 +0900

    66 Setting styles Dynamically

--- course/src/App.js
+++ course/src/App.js
@@ -46,10 +46,12 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
       font: "inherit",
+      color: "white",
       border: "1px solid blue",
       padding: "8px",
+      cursor: "pointer",
     };
 
     let persons = null;
@@ -69,7 +71,9 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = "red";
     }
+
     return (
       <div className="App">
         <h1>Hi, im react</h1>
