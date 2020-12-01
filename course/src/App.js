b4ae326abf92621996c6c1d85b5b5e3a769cfe94
Brandon Ha <hsy0602@outlook.com>
2020-12-01 12:39:14 +0900

    57 List & state

--- course/src/App.js
+++ course/src/App.js
@@ -13,14 +13,10 @@ class App extends Component {
     showPersons: false,
   };
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 24 },
-        { name: "Manu", age: 20 },
-        { name: "emily", age: 27 },
-      ],
-    });
+  deletePersonHandler = (PersonIndex) => {
+    const persons = this.state.persons;
+    persons.splice(PersonIndex, 1);
+    this.setState({ persons: persons });
   };
 
   nameChangedHandler = (event) => {
@@ -50,8 +46,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person) => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+              />
+            );
           })}
         </div>
       );
