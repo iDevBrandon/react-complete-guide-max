63984e99cda382038f111cd2b89c20ef06a0db4d
Brandon Ha <hsy0602@outlook.com>
2020-12-02 20:45:15 +0900

    88 Class-based vs Funtional components
    prop anywhere by setting in index.js <App>

--- course/src/components/Cockpit/Cockpit.js
+++ course/src/components/Cockpit/Cockpit.js
@@ -18,7 +18,7 @@ const cockpit = (props) => {
 
   return (
     <div className="classes.Cockpit">
-      <h1>Hi, im react</h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}> this is really working</p>
       <button className={btnClass} onClick={props.clicked}>
         Switch name
--- course/src/containers/App.js
+++ course/src/containers/App.js
@@ -62,6 +62,7 @@ class App extends Component {
     return (
       <div className="App">
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
