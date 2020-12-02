71130fa274b6aebaab6967f565b10d3468784e48
Brandon Ha <hsy0602@outlook.com>
2020-12-02 22:45:48 +0900

    90 component creation lifecycle in action

--- course/src/containers/App.js
+++ course/src/containers/App.js
@@ -6,6 +6,11 @@ import Persons from "../components/Persons/Persons";
 //import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
   state = {
     persons: [
       { id: "as2", name: "Max", age: 24 },
@@ -15,6 +20,15 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
 
   deletePersonHandler = (PersonIndex) => {
     //const persons = this.state.persons.slice(); or
@@ -48,6 +62,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
     let persons = null;
     if (this.state.showPersons) {
       persons = (
