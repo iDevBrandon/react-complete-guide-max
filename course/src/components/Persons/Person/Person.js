5e13e54e4c54c6b6bccf3900b6c39a99fa25869d
Brandon Ha <hsy0602@outlook.com>
2020-12-02 23:19:07 +0900

    91 component update lifecycle(for props changes)

--- course/src/components/Persons/Person/Person.js
+++ course/src/components/Persons/Person/Person.js
@@ -1,30 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Person.css";
 
-// const StyledDiv = styled.div`
-//   width: 60%;
-//   margin: 16px auto;
-//   border: 1px solid #eee;
-//   box-shadow: 0 2px 3px #ccc;
-//   padding: 16px;
-//   text-align: center;
-
-//   @media (min-width: 500px) {
-//     width: 450px;
-//   }
-// `;
-
-const Person = (props) => {
-  console.log("[Person.js] rendering...");
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm {props.name} and {props.age} years old
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component {
+  render() {
+    console.log("[Person.js] rendering...");
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and {this.props.age} years old
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </div>
+    );
+  }
+}
 
 export default Person;
