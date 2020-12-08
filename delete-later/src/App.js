84ed5bdd5878d14ba3ff319180d3c455ad5e6601
Brandon Ha <hsy0602@outlook.com>
2020-12-08 15:24:04 +0900

    128 Starting the burger component

--- delete-later/src/App.js
+++ delete-later/src/App.js
@@ -1,14 +1,19 @@
-import "./App.css";
+import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <p>hey</p>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+ 
--- delete-later/src/components/Layout/Layout.js
+++ delete-later/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import classes from "./../../containers/BurgerBuilder/BurgerBuilder.module.css";
 
 const Layout = (props) => {
   return (
     <Aux>
       <div>Toolbar, Side, backdrop</div>
-      <main>{props.children}</main>
+      <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
 };
