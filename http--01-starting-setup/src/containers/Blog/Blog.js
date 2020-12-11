ffefa17b6f95d6a9929f53f609cf8ca4d22be2ab
Brandon Ha <hsy0602@outlook.com>
2020-12-11 16:00:20 +0900

    195 Switching between pages

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 //import axios from "axios";
-import Posts from "./Posts/Posts";
 import { Route } from "react-router-dom";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
 //import Post from "../../components/Post/Post";
 
@@ -29,6 +30,7 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home2</h1>} /> */}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
