cf87b1da3dcc25692bc3a95a4ab3ca34a953313c
Brandon Ha <hsy0602@outlook.com>
2020-12-14 11:41:21 +0900

    209. Creating Dynamic Nested Routes

--- http--01-starting-setup/src/containers/Blog/FullPost/FullPost.js
+++ http--01-starting-setup/src/containers/Blog/FullPost/FullPost.js
@@ -8,12 +8,11 @@ class FullPost extends Component {
     loadedPost: null,
   };
 
-  componentDidMount() {
-    console.log(this.props);
+  loadData() {
     if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
-        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+        (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)
       ) {
         axios.get("/posts/" + this.props.match.params.id).then((response) => {
           //console.log(response);
@@ -23,8 +22,17 @@ class FullPost extends Component {
     }
   }
 
+  componentDidMount() {
+    console.log(this.props);
+    this.loadData();
+  }
+
+  componentDidUpdate() {
+    this.loadData();
+  }
+
   deletePostHandler = () => {
-    axios.delete("/posts/" + this.props.id).then((response) => {
+    axios.delete("/posts/" + this.props.match.params.id).then((response) => {
       console.log(response);
     });
   };
@@ -32,7 +40,7 @@ class FullPost extends Component {
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
 
-    if (this.props.id) {
+    if (this.props.match.params.id) {
       post = <p style={{ textAlign: "center" }}>Loading...</p>;
     }
 
